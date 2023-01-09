af214da4ce9bd993f803c58617a2b763cfadb671
scar <allyyydelrosario@gmail.com>
2023-01-09 09:49:40 +0800

    Update 2023_01_03_075028_create_employees_table.php

--- database/migrations/2023_01_03_075028_create_employees_table.php
+++ database/migrations/2023_01_03_075028_create_employees_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id()->startingValue(10000);
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
