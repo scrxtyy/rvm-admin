57cbbd9c623d00991191e6b382595d81fa984296
scar <allyyydelrosario@gmail.com>
2023-06-08 19:03:36 +0800

    created records controller

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -49,6 +49,7 @@
                   focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
                 ">
                 
+                
                   <button type="submit" class="inline-block px-6 py-2.5 m-3 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
                     Search
                   </button>
